698638757bb4ff71fd62d1bd784b1c39eab281e1
wieup <z.sardaana@yandex.ru>
2025-12-15 21:53:37 +0700

    Initial commit: project structure

---
+++ PhotoEditor/Models/Photo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PhotoEditor
+{
+    public class Photo
+    {
+        public string Name { get; set; }
+
+        public Photo(string name)
+        {
+            Name = name;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Фото: {Name}");
+        }
+    }
+}
---
+++ PhotoEditor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PhotoEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Photo Editor ===");
+            List<Photo> photos = new List<Photo>();
+
+            photos.Add(new Photo("example1.jpg"));
+            photos.Add(new Photo("example2.jpg"));
+
+            foreach (var photo in photos)
+            {
+                photo.Display();
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
