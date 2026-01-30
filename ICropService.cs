0e67f1a4c0a7b4b99906f70a97d6ec8dd3968a19
tao <pilageinat@gmail.com>
2026-01-31 01:12:31 +0500

    Удаленине ненужных файлов

--- ICropService.cs
+++
@@ -1,9 +0,0 @@
-using System.Windows.Media.Imaging;
-
-namespace PictureEditor.Interfaces
-{
-    public interface ICropService
-    {
-        BitmapSource Crop(BitmapSource source, int x, int y, int width, int height);
-    }
-}
